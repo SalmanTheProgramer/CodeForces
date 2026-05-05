eda6cdd446425542fe40c3b9ae8ddf6791f062f5
SalmanTheProgramer <msk.99401@gmail.com>
2026-05-05 13:12:29 +0600

    committed

--- P_Shape_1.c
+++ P_Shape_1.c
@@ -5,15 +5,30 @@ int main() {
     int n;
     scanf ("%d", &n);
 
-    for (int i=n; i>=1; i--) {
+    // for (int i=n; i>=1; i--) {
 
-        for (int j=1; j<=i; j++) {
+    //     for (int j=1; j<=i; j++) {
+
+    //         printf ("*");
+
+    //     }
+
+    //     printf ("\n");
+    // }
+
+    int star = n;
+
+    for (int i=1; i<=n; i++) {
+
+        for (int j=1; j<=star; j++) {
 
             printf ("*");
 
         }
 
         printf ("\n");
+
+        star--;
     }
     
     return 0;

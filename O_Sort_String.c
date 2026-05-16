ff2f6b470120044afd2e8dd292c2ed499fe5441e
SalmanTheProgramer <msk.99401@gmail.com>
2026-05-16 16:57:57 +0600

    O. Sort String
    link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/O

--- O_Sort_String.c
+++ O_Sort_String.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-void stringSort (int n, char s[]) {
-
-    for (int i=0; i<n-1; i++) {
-
-        for (int j=i+1; j<n; j++) {
-
-            if (s[i] > s[j]) {
-
-                char temp = s[i];
-                    s[i] = s[j];
-                    s[j] = temp;
-            }
-        }
-    }
-
-    printf ("%s", s);
-}
 
 int main() {
     
     int n;
     scanf ("%d", &n);
 
-    // allocate on heap
-    char *str = (char *) malloc ((n+1) * sizeof (char));
-    scanf ("%s", str);
+    // dynamic memory allocation
+    char *s = (char *) malloc ((n + 1) * sizeof (char));
+    scanf ("%s", s);
 
-    stringSort (n, str);
+    int freq[26] = {0};
 
-    free (str); // free memory
+    // count freq. of each char.
+    for (int i=0; i<n; i++) {
+
+        freq[s[i] - 'a']++;
+    }
+
+    // print sorted string
+    for (int i=0; i<26; i++) {
+
+        for (int j=0; j<freq[i]; j++) {
+
+            printf ("%c", 'a' + i);
+        }
+    }
+
+    free (s);
     
     return 0;
 }
